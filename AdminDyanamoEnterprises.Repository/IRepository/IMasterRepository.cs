b122048517ac5082b9c13d0cf2a6abe825e08246
bashat450 <parweenbashat@gmail.com>
2025-06-20 17:03:50 +0530

    Changes

--- AdminDyanamoEnterprises.Repository/IRepository/IMasterRepository.cs
+++ AdminDyanamoEnterprises.Repository/IRepository/IMasterRepository.cs
@@ -1,5 +1,4 @@
 ﻿using AdminDyanamoEnterprises.DTOs;
-using AdminDyanamoEnterprises.DTOs.Common;
 using AdminDyanamoEnterprises.DTOs.Master;
 using System;
 using System.Collections.Generic;
@@ -12,24 +11,45 @@ namespace AdminDyanamoEnterprises.Repository
 {
     public interface IMasterRepository
     {
-       
-        public MasterResponse InsertorUpdateCategoryType(CategoryTypePageViewModel addCategoryType);
-        public List<CategoryType>GetAllCategoryType();
+
+        public void InsertorUpdateCategoryType(CategoryTypePageViewModel addCategoryType);
+        public List<CategoryType> GetAllListType();
 
         public void DeleteCategory(int id);
-        public void InsertOrUpdateOrDeletePattern(PatternTypePageViewModel addPatternType);
-
-        public List<PatternType> GetAllPatternType();
-
-        public void DeletePattern(int id);
-
-
-
-        public void InsertOrUpdateSubCategory(SubAddCategoryType model);
-        public void DeleteSubCategory(int subCategoryId);
-
-        public List<SubCategoryType> GetAllSubCategoriesWithCategoryName();
 
 
     }
+
+    #region==================IColor Repository==============================================
+    public interface IColorRepository
+    {
+        List<ColorType> GetAllListColorType();
+
+        string Sp_InsertOrUpdateOrDeleteColor(ColorTypePageViewModel colorType);
+
+        string DeleteColor(int id);
+    }
+    #endregion
+
+    #region==================IFabric Repository==============================================
+    public interface IFabricRepository
+    {
+        List<FabricType> GetAllListFabricType();
+
+        string Sp_InsertOrUpdateOrDeleteFabric(FabricTypePageViewModel fabricType);
+
+        string DeleteFabric(int id);
+    }
+    #endregion
+    #region==================IMaterial Repository==============================================
+    public interface IMaterialRepository
+    {
+        List<MaterialType> GetAllListMaterialType();
+
+        string Sp_InsertOrUpdateOrDeleteMaterialType(MaterialTypePageViewModel materialType);
+
+        string DeleteMaterial(int id);
+    }
+    #endregion
+
 }

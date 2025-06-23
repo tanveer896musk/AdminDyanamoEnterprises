ccec6c31301d7dbc80505db5c83cb760add8c6d5
ARIF CHOUDHARY <choudharyarif2398@gmail.com>
2025-06-23 11:47:30 +0530

    commit by Arif

---
+++ AdminDyanamoEnterprises.Repository/Repository/AccountRepositary.cs
@@ -0,0 +1,14 @@
+﻿using AdminDyanamoEnterprises.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdminDyanamoEnterprises.Repository.Repository
+{
+    public class AccountRepositary:IAccountRepositary
+    {
+
+    }
+}

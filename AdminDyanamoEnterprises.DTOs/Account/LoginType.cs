7e3cc7dadc86059ee122b603abe0f9b240be8524
ARIF CHOUDHARY <choudharyarif2398@gmail.com>
2025-06-24 16:55:51 +0530

    commit by Arif

--- AdminDyanamoEnterprises.DTOs/Account/LoginType.cs
+++ AdminDyanamoEnterprises.DTOs/Account/LoginType.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,8 @@ namespace AdminDyanamoEnterprises.DTOs
     public class LoginType
     {
         public int Userid { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
         public string? Emailid { get; set; }
         
         public string? Password { get; set; }

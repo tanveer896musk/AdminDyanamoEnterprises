d43c9b3d7faded40ebacfd8fd02c994787c4608c
Narayan Sharma <narayanvgsharma99@gmail.com>
2025-06-25 12:29:22 +0530

    Commited By Tanveeer

--- AdminDyanamoEnterprises.DTOs/Master/BannerImage.cs
+++ AdminDyanamoEnterprises.DTOs/Master/BannerImage.cs
@@ -7,7 +7,8 @@ using System.Threading.Tasks;
 namespace AdminDyanamoEnterprises.DTOs.Master
 {
     public class BannerImage
-    {
+    {   
+        //comment
         public int BannerID { get; set; }
         public string Image { get; set; }
         public DateTime CreatedDate { get; set; }
